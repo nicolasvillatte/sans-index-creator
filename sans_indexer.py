0dad232333d9722e0b21f6a2396785af3a4df5fa
Nicolas Villatte <nicolas.villatte@gmail.com>
2023-04-02 09:15:29 +0200

    Update sans_indexer.py
    fixing utf-8 write issue

--- sans_indexer.py
+++ sans_indexer.py
@@ -115,7 +115,7 @@ for word in set(total_words):
 results.sort(key=str.casefold)
 
 # Write output to file
-with open(options.output_file, "w") as f:
+with open(options.output_file, "w", encoding='utf8') as f:
     for result in results:
         f.write(result + "\n")
 print(f"Written index to {options.output_file}")
